f81ea9339ca0e1540dbcb836fe08c985b4f2283e
Mit-Darji <107803981+Mit-Darji@users.noreply.github.com>
2023-06-05 15:04:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max sum in sub-arrays/max-sum-in-subarrays.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long pairWithMaxSum(long long arr[], long long n)
+    {
+        // Your code goes here
+        long long int maxi = 0;
+        long long int sum = 0;
+        for(int i = 0;i < n - 1;i++){
+            sum = arr[i] + arr[i + 1];
+            maxi =  max(maxi,sum);
+        }
+        return maxi;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+	    for(ll i=0;i<n;i++)
+		    cin>>a[i];
+		Solution ob;
+	    cout<<ob.pairWithMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
